8cd4408b568821aea7aea8adaafb7a6086699fa6
Evan Pavone <evanrpavone@gmail.com>
2020-09-07 15:18:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,4 +21,11 @@ class App < Sinatra::Base
     "#{answer}"
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    all = params.map do |k,v|
+      v
+    end
+    "#{all.join(" ")}."
+  end
+
 end
